bf31ac03466092fe7251afc3afe2cae14c0c04b7
artBatamirov <119311223+artBatamirov@users.noreply.github.com>
2024-05-01 00:02:33 +0300

    Update add_task_form.py

--- forms/add_task_form.py
+++ forms/add_task_form.py
@@ -7,7 +7,7 @@ import datetime
 
 class TaskForm(FlaskForm):
     title = StringField('Название', validators=[DataRequired()])
-    description = TextAreaField('Оиписание')
+    description = TextAreaField('Описание')
     category = SelectField('Категория', choices=['спорт', 'обучение', 'развлечения', 'программирование'])
     date_time = DateTimeLocalField('Дата и время', validators=[DataRequired()])
     importance = SelectField("Важность", choices=['низкая', 'средняя', 'высокая'], default='низкая')
